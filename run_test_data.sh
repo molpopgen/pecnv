855d513797c5e83ea357f6bbf9bd1484d7c568b0
krthornt <krthornt@hinault.(none)>
2014-02-19 17:08:13 -0800

    Added main body

--- run_test_data.sh
+++ run_test_data.sh
@@ -1,3 +1,11 @@
 #!sh
 
-#Runs 
+wget http://hpc.oit.uci.edu/~krthornt/references/dyak/dyak-all-chromosome-r1.3-newnames.fasta
+
+bwa index dyak-all-chromosome-r1.3-newnames.fasta
+
+wget http://hpc.oit.uci.edu:~/krthornt/dyak_genomic_data/NY4_*
+
+ls -1 *.fastq.gz | sort > infile
+
+pecnv.pl -infile infile -ref dyak-all-chromosome-r1.3-newnames.fasta -sample 0
