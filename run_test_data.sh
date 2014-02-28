3b33fa08bfed7ac2e4fc414e3856521769a9bf7f
Kevin R. Thornton <krthornt@uci.edu>
2014-02-28 09:15:02 -0800

    checks for curl vs wget now.  Silly OS X....

--- run_test_data.sh
+++ run_test_data.sh
@@ -3,7 +3,35 @@
 #Change this if your system has < 32 CPU
 CPU=32
 
-wget http://hpc.oit.uci.edu/~krthornt/references/dyak/dyak-all-chromosome-r1.3-newnames.fasta
+command_exists () {
+    type "$1" &> /dev/null ;
+}
+
+WGET=0
+CURL=0
+
+if command_exists wget
+then
+    WGET=1
+fi
+
+if command_exists curl
+then 
+    CURL=1
+fi
+
+if [ $WGET -eq 0 ] && [ $CURL -eq 0 ]
+then
+    echo "Error: neither wget nor curl were found in your \$PATH"
+    exit 10;
+fi
+
+if [ $WGET -eq 1 ]
+then
+    wget http://hpc.oit.uci.edu/~krthornt/references/dyak/dyak-all-chromosome-r1.3-newnames.fasta
+else
+    curl http://hpc.oit.uci.edu/~krthornt/references/dyak/dyak-all-chromosome-r1.3-newnames.fasta -o dyak-all-chromosome-r1.3-newnames.fasta
+fi
 
 bwa index dyak-all-chromosome-r1.3-newnames.fasta
 
