a828872db1238f27f654b8f0a2177a6b511cff7d
krthornt <krthornt@uci.edu>
2014-11-13 17:47:22 -0800

    The various operations all work without crashing at this point

--- src/teclust_phrapify.cc
+++ src/teclust_phrapify.cc
@@ -67,7 +67,8 @@ vector<clusteredEvent> parseClusters(const string & clusters,const params & pars
 
 using LeftRights = pair<unordered_set<string>,
 			unordered_set<string> >;
-using ReadCollection = vector< LeftRights >;
+//using ReadCollection = vector< LeftRights >;
+using ReadCollection = unordered_set<string>;
 using PhrapInput = vector< pair<pair< vector<Fasta>, vector<Fasta> >,
 				pair< vector<Fasta>, vector<Fasta> > > >;
 
@@ -121,7 +122,7 @@ getRnames( const params & pars,
       return false;
     };
 
-  ReadCollection rv( cEs.size() );
+  ReadCollection rv;//( cEs.size() );
   while(!reader.eof() && !reader.error())
     {
       bamrecord b = reader.next_record();
@@ -135,21 +136,22 @@ getRnames( const params & pars,
 	  //of a (filtered) cluster?
 	  auto cluster  = find_if(cEs.cbegin(),cEs.cend(),
 				  bind(Cfinder,placeholders::_1,bf.qstrand,b.pos(),pars.INSERTSIZE,chrom,&side));
-	  while( cluster != cEs.cend() )
+	  if( cluster != cEs.cend() )
 	    {
-	      auto n = editRname(b.read_name());
-	      auto rvitr = rv.begin() + (cluster-cEs.cbegin());
-	      if( !side && rvitr->first.find(n) == rvitr->first.end())
-		{
-		  rvitr->first.insert(n);
-		}
-	      else if(side==1 && rvitr->second.find(n) == rvitr->second.end())
-		{
-		  rvitr->second.insert(n);
-		}
-	      side = -1;
-	      cluster = find_if(cluster+1,cEs.cend(),
-				 bind(Cfinder,placeholders::_1,bf.qstrand,b.pos(),pars.INSERTSIZE,chrom,&side));
+	      //auto n = editRname(b.read_name());
+	      rv.insert(editRname(b.read_name()));
+	      //auto rvitr = rv.begin() + (cluster-cEs.cbegin());
+	      // if( !side && rvitr->first.find(n) == rvitr->first.end())
+	      // 	{
+	      // 	  rvitr->first.insert(n);
+	      // 	}
+	      // else if(side==1 && rvitr->second.find(n) == rvitr->second.end())
+	      // 	{
+	      // 	  rvitr->second.insert(n);
+	      // 	}
+	      // side = -1;
+	      // cluster = find_if(cluster+1,cEs.cend(),
+	      // 			 bind(Cfinder,placeholders::_1,bf.qstrand,b.pos(),pars.INSERTSIZE,chrom,&side));
 	    }
 	}
     }
@@ -157,7 +159,8 @@ getRnames( const params & pars,
   return rv;
 }
 
-PhrapInput seqQual( const params & pars, const ReadCollection & r )
+PhrapInput seqQual( const params & pars, const vector<clusteredEvent> & cEs,
+		    const ReadCollection & r )
 {
   bamreader reader(pars.bamfile.c_str());
 
@@ -169,58 +172,126 @@ PhrapInput seqQual( const params & pars, const ReadCollection & r )
       exit(1);
     }
 
-  auto FindCollection = [](const LeftRights & __lr, 
-			   const string & n,
-			   bool * isleft) 
+  // auto FindCollection = [](const LeftRights & __lr, 
+  // 			   const string & n,
+  // 			   bool * isleft) 
+  //   {
+  //     if ( __lr.first.find(n) != __lr.first.cend() )
+  // 	{
+  // 	  return true;
+  // 	}
+  //     else if ( __lr.second.find(n) != __lr.second.cend() )
+  // 	{
+  // 	  *isleft = false;
+  // 	  return true;
+  // 	}
+  //     return false;
+  //   };
+
+  auto Cfinder = [](const clusteredEvent & __cE,
+		    const bool & strand,
+		    const int32_t & pos,
+		    const unsigned & INSERTSIZE,
+		    const string & chrom,
+		    int * side)
     {
-      if ( __lr.first.find(n) != __lr.first.cend() )
+      if(__cE.chrom != chrom) return false;
+      if( !strand ) //if read is on + strand
 	{
-	  return true;
+	  //If read is 5' of this cluster's end
+	  if( pos < __cE.plast &&
+	      __cE.pfirst != -1 && __cE.plast != -1 &&
+	      ( abs(pos-__cE.pfirst) <= INSERTSIZE ||
+		abs(pos-__cE.plast) <= INSERTSIZE ) )
+	    {
+	      *side = 0; //LEFT
+	      return true;
+	    }
 	}
-      else if ( __lr.second.find(n) != __lr.second.cend() )
+      else if (strand) //read is on - strand
 	{
-	  *isleft = false;
-	  return true;
+	  //If read is 3' of this cluster's start
+	  if( pos > __cE.mfirst &&
+	      __cE.mfirst != -1 && __cE.mlast != -1 &&
+	      ( abs(pos-__cE.mfirst) <= INSERTSIZE ||
+		abs(pos-__cE.mlast) <= INSERTSIZE ) )
+	    {
+	      *side = 1; //RIGHT
+	      return true;
+	    }
 	}
       return false;
     };
 
-  PhrapInput rv(r.size());
+  PhrapInput rv(cEs.size());
   while(!reader.eof() && !reader.error())
     {
       bamrecord b = reader.next_record();
       if(b.empty()) break;
-      samflag bf(b.flag());
-
-      bool isleft = true;
       string n = editRname(b.read_name());
-      auto itr = find_if( r.cbegin(),
-			  r.cend(),
-			  bind(FindCollection,placeholders::_1,n,&isleft) );
-      //if( itr != r.cend() )
-      while( itr != r.cend() )
+      if ( r.find(n) != r.end() )
 	{
-	  string qstring;
-	  for_each(b.qual_cbeg(),b.qual_cend(),
-		   [&](const int8_t & __i) {
-		     qstring += (__i+33) + ' ';
-		   });
-	  auto rvItr = (rv.begin() + size_t(itr-r.cbegin()));
-	  if (isleft)
+	  samflag bf(b.flag()); 
+	  int side = -1;//-1 = not known, 0 = left, 1 = right
+	  string chrom = (reader.ref_cbegin() + b.refid())->first; //Get the string representing this read's chromosome
+	  //Is the alignment start position of this read w/in ISIZE
+	  //of a (filtered) cluster?
+	  auto cluster  = find_if(cEs.cbegin(),cEs.cend(),
+				  bind(Cfinder,placeholders::_1,bf.qstrand,b.pos(),pars.INSERTSIZE,chrom,&side));
+	  while( cluster != cEs.cend() )
 	    {
-	      rvItr->first.first.emplace_back(Fasta(n,b.seq()));
-	      rvItr->first.second.emplace_back(Fasta(n,move(qstring)));
+	      auto rvitr = rv.begin() + (cluster-cEs.cbegin());
+	       	  string qstring;
+		  for_each(b.qual_cbeg(),b.qual_cend(),
+			   [&](const int8_t & __i) {
+			     qstring += to_string(int(__i+33)) + ' ';
+			   });
+	      if(side==0)
+		{
+		  rvitr->first.first.push_back(Fasta(n,b.seq()));
+		  rvitr->first.second.push_back(Fasta(n,qstring));
+		}
+	      else if (side == 1)
+		{
+		  rvitr->second.first.push_back(Fasta(n,b.seq()));
+		  rvitr->second.second.push_back(Fasta(n,qstring));
+		}
+	      side = -1;
+	      cluster = find_if(cluster+1,cEs.cend(),
+				bind(Cfinder,placeholders::_1,bf.qstrand,b.pos(),pars.INSERTSIZE,chrom,&side));
 	    }
-	  else
-	    {
-	      rvItr->second.first.emplace_back(Fasta(n,b.seq()));
-	      rvItr->second.second.emplace_back(Fasta(n,move(qstring)));
-	    }
-	  isleft = true;
-	  itr = find_if(itr+1,
-			r.cend(),
-			bind(FindCollection,placeholders::_1,n,&isleft) );
 	}
+      // samflag bf(b.flag());
+
+      // bool isleft = true;
+      // string n = editRname(b.read_name());
+      // auto itr = find_if( r.cbegin(),
+      // 			  r.cend(),
+      // 			  bind(FindCollection,placeholders::_1,n,&isleft) );
+      // //if( itr != r.cend() )
+      // while( itr != r.cend() )
+      // 	{
+      // 	  string qstring;
+      // 	  for_each(b.qual_cbeg(),b.qual_cend(),
+      // 		   [&](const int8_t & __i) {
+      // 		     qstring += (__i+33) + ' ';
+      // 		   });
+      // 	  auto rvItr = (rv.begin() + size_t(itr-r.cbegin()));
+      // 	  if (isleft)
+      // 	    {
+      // 	      rvItr->first.first.emplace_back(Fasta(n,b.seq()));
+      // 	      rvItr->first.second.emplace_back(Fasta(n,move(qstring)));
+      // 	    }
+      // 	  else
+      // 	    {
+      // 	      rvItr->second.first.emplace_back(Fasta(n,b.seq()));
+      // 	      rvItr->second.second.emplace_back(Fasta(n,move(qstring)));
+      // 	    }
+      // 	  isleft = true;
+      // 	  itr = find_if(itr+1,
+      // 			r.cend(),
+      // 			bind(FindCollection,placeholders::_1,n,&isleft) );
+      // 	}
     }
   return rv;
 }
@@ -230,7 +301,7 @@ string baseName(const string & basedir,
 		const int32_t & first,
 		const int32_t & last )
 {
-  string n = basedir + "/" + chrom + to_string(first) + to_string(last) + ".fasta";
+  string n = basedir + "/" + chrom + "." + to_string(first) + "." + to_string(last) + ".fasta";
   return n;
 }
 
@@ -305,7 +376,7 @@ void phrapify( const params & pars,
 
   //Get the sequences and quality scores for all sequences for both sides
   cerr << "Retrieving sequences and quality scores...\n";
-  auto sq = seqQual(pars,LR);
+  auto sq = seqQual(pars,cEs,LR);
 
   //Output
   cerr << "Output...\n";
