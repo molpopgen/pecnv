7b042fa43505800e789f974bfabbf0b784b59323
krthornt <krthornt@uci.edu>
2014-11-19 14:58:32 -0800

    added .R

--- test/TEsThreeWays.sh
+++ test/TEsThreeWays.sh
@@ -35,6 +35,6 @@ teclust -u TEtestData/pecnv_output/pecnv_bamfile.um_u.csv.gz -m TEtestData/pecnv
 ##Method 4: using the UMU/UMM files, the TE annotation file, and require that Ms in UMM overlap known TEs.  Include bam file scanning
 teclust -u TEtestData/pecnv_output/pecnv_bamfile.um_u.csv.gz -m TEtestData/pecnv_output/pecnv_bamfile.um_m.csv.gz -t TEtestData/TE_position_r5.1 --ummHitTE -b TEtestData/pecnv_output/pecnv_bamfile_sorted.bam -i `pecnv_insert_qtile TEtestData/pecnv_output/pecnv_bamfile.mdist.gz 0.99` -o method4.out.gz
 
-./checkTEoutput method2.out.gz TEtestData/TE_position_r5.1 TEtestData/line99_truth method2.compare.out
-./checkTEoutput method3.out.gz TEtestData/TE_position_r5.1 TEtestData/line99_truth method3.compare.out
-./checkTEoutput method4.out.gz TEtestData/TE_position_r5.1 TEtestData/line99_truth method4.compare.out
+./checkTEoutput.R method2.out.gz TEtestData/TE_position_r5.1 TEtestData/line99_truth method2.compare.out
+./checkTEoutput.R method3.out.gz TEtestData/TE_position_r5.1 TEtestData/line99_truth method3.compare.out
+./checkTEoutput.R method4.out.gz TEtestData/TE_position_r5.1 TEtestData/line99_truth method4.compare.out
