8c0dcc30b2279226b8ebfcdd70fe7906301fa91c
Kevin R. Thornton <krthornt@uci.edu>
2014-11-24 16:51:37 -0800

    fewer includes

--- src/common.hpp
+++ src/common.hpp
@@ -2,8 +2,6 @@
 #define __PECNV_COMMON_HPP__
 
 #include <string>
-#include <vector>
-#include <utility>
 
 //Turn a read name from XXX#something to XXX
 std::string editRname( const std::string & readname );
