6962bdbe44ec57537086f93a06cf841818fc63d5
Kevin R. Thornton <krthornt@uci.edu>
2014-11-24 17:21:36 -0800

    added

---
+++ src/mkgenome.cc
@@ -0,0 +1,14 @@
+#include <Sequence/Fasta.hpp>
+#include <boost/program_options.hpp>
+
+using namespace std;
+using namespace boost::program_options;
+using namespace Sequence;
+
+int mkgenome_main( int argc, char ** argv )
+{
+  string infile,outfie;
+  return 0;
+}
+
+
