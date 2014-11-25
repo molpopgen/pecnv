995184d7cd57d2a5199616e4761ad580cfaa7340
Kevin R. Thornton <krthornt@uci.edu>
2014-11-24 18:11:54 -0800

    added is_gzip

--- src/file_common.cc
+++ src/file_common.cc
@@ -1,7 +1,21 @@
 #include <sys/stat.h>
+#include <fstream>
+
+using namespace std;
 
 int file_exists(const char * fn)
 {
   struct stat buf;
   return (stat(fn,&buf) != -1);
+}
+
+int is_gzip(const char * fn)
+{
+  ifstream in(fn);
+  if(!in) return 0;
+  int x;
+  in.read(reinterpret_cast<char*>(&x),sizeof(int));
+  auto first = x & 0xFF;
+  auto second = ((x >> (8*1)) & 0xFF);
+  return (first==0x1f) && (second==0x8b);
 }
