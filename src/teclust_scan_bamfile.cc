bf81ce4eedd5f8e01549b737450cd35b5acee79c
krthornt <krthornt@uci.edu>
2015-02-07 13:53:56 -0800

    protypes up top

--- src/teclust_scan_bamfile.cc
+++ src/teclust_scan_bamfile.cc
@@ -19,95 +19,13 @@ using refIDlookup = unordered_map<int32_t,string>;
 
 //DEFINITION OF FUNCTIONS
 refIDlookup make_lookup(const bamreader & reader);
-
-  //SECOND PASS -- new version based on seeking within the bgzf file
-
-vector< pair<int64_t,int64_t> > group_offsets( std::set<pair<int64_t,int64_t> > & OFFSETS )
-{
-  vector< pair<int64_t,int64_t> >voffsets(OFFSETS.begin(),OFFSETS.end());
-  OFFSETS.clear();
-  vector< pair<int64_t,int64_t> >::size_type ii = 0;
-  while(ii < voffsets.size())
-    {
-      int64_t a = voffsets[ii].first,b = voffsets[ii].second;
-      auto itr = remove_if( voffsets.begin() + ii + 1, voffsets.end(),
-			    [&a,&b]( pair<int64_t,int64_t> & __p ) {
-			      bool A = (a >= __p.first && a <= __p.second),
-			      B = (b >= __p.first && b <= __p.second),
-			      C = (__p.first >= a && __p.first <= b),
-			      D = (__p.second >= a && __p.second <= b);
-			      return A||B||C||D;
-			    } );
-      if(itr == voffsets.end())++ii;
-      else
-	{
-	  for_each(itr,voffsets.end(),
-		   [&voffsets,ii](const pair<int64_t,int64_t> & __p ) {
-		     voffsets[ii].first = min({voffsets[ii].first,__p.first,voffsets[ii].second,__p.second}
-					      );
-		     voffsets[ii].second = max({voffsets[ii].first,__p.first,voffsets[ii].second,__p.second
-			   });
-		   }
-		   );
-	  voffsets.erase(itr,voffsets.end());
-	}
-    }  
-  return voffsets;
-}
-
 void second_scan( const refTEcont & refTEs,
 		  const refIDlookup & lookup,
 		  const vector<pair<int64_t,int64_t> > & voffsets,
 		  const unordered_map<string,pair<int32_t,int32_t> > & RPlocal,
 		  map<string,vector< puu > > * data,
-		  bamreader & reader)
-{
-  for( auto i = voffsets.cbegin() ; i < voffsets.cend() ; ++i )
-    {
-      reader.seek(i->first,SEEK_SET);
-      while( reader.tell() <= i->second )
-	{
-	  bamrecord b = reader.next_record();
-	  samflag f(b.flag());
-	  if(!f.query_unmapped && !f.mate_unmapped)
-	    {
-	      string n = editRname(b.read_name());
-	      auto RPitr = RPlocal.find(n);
-	      if(RPitr != RPlocal.end() && 
-		 RPitr->second.first == b.refid() && 
-		 RPitr->second.second == b.pos())
-		{
-		  auto itr = lookup.find(b.refid());
-   		  int32_t start = b.pos(),stop=b.pos() + alignment_length(b) - 1;
-   		  auto CHROM = refTEs.find(itr->second);
-   		  if( CHROM != refTEs.end() )
-   		    {
-   		      bool hitsTE = find_if( CHROM->second.cbegin(),
-   					     CHROM->second.cend(),
-   					     [&](const teinfo & __t) {
-   					       bool A = (start >= __t.start() && start <= __t.stop());
-   					       bool B = (stop >= __t.start() && stop <= __t.stop());
-   					       return A||B;
-   					     }) != CHROM->second.cend();
-   		      if(!hitsTE)
-   			{
-   			  auto DCHROM = data->find(itr->second);
-  			  if(DCHROM == data->end())
-   			    {
-   			      data->insert( make_pair(itr->second,
-   						      vector<pair<int32_t,int8_t> >(1,make_pair(start,f.qstrand) ) ) );
-   			    }
-   			  else
-   			    {
-   			      DCHROM->second.emplace_back(make_pair(start,f.qstrand));
-   			    }
-   			}
-   		    }
-		}
-	    }
-	}
-    }
-}
+		  bamreader & reader);
+vector< pair<int64_t,int64_t> > group_offsets( std::set<pair<int64_t,int64_t> > & OFFSETS );
 
 void scan_bamfile(const teclust_params & p,
 		  const refTEcont & refTEs,
@@ -217,4 +135,91 @@ make_lookup(const bamreader & reader)
 	     rv[I++]=__p.first;
 	   });
   return rv;
+}
+
+vector< pair<int64_t,int64_t> > group_offsets( std::set<pair<int64_t,int64_t> > & OFFSETS )
+{
+  vector< pair<int64_t,int64_t> >voffsets(OFFSETS.begin(),OFFSETS.end());
+  OFFSETS.clear();
+  vector< pair<int64_t,int64_t> >::size_type ii = 0;
+  while(ii < voffsets.size())
+    {
+      int64_t a = voffsets[ii].first,b = voffsets[ii].second;
+      auto itr = remove_if( voffsets.begin() + ii + 1, voffsets.end(),
+			    [&a,&b]( pair<int64_t,int64_t> & __p ) {
+			      bool A = (a >= __p.first && a <= __p.second),
+			      B = (b >= __p.first && b <= __p.second),
+			      C = (__p.first >= a && __p.first <= b),
+			      D = (__p.second >= a && __p.second <= b);
+			      return A||B||C||D;
+			    } );
+      if(itr == voffsets.end())++ii;
+      else
+	{
+	  for_each(itr,voffsets.end(),
+		   [&voffsets,ii](const pair<int64_t,int64_t> & __p ) {
+		     voffsets[ii].first = min({voffsets[ii].first,__p.first,voffsets[ii].second,__p.second}
+					      );
+		     voffsets[ii].second = max({voffsets[ii].first,__p.first,voffsets[ii].second,__p.second
+			   });
+		   }
+		   );
+	  voffsets.erase(itr,voffsets.end());
+	}
+    }  
+  return voffsets;
+}
+
+void second_scan( const refTEcont & refTEs,
+		  const refIDlookup & lookup,
+		  const vector<pair<int64_t,int64_t> > & voffsets,
+		  const unordered_map<string,pair<int32_t,int32_t> > & RPlocal,
+		  map<string,vector< puu > > * data,
+		  bamreader & reader)
+{
+  for( auto i = voffsets.cbegin() ; i < voffsets.cend() ; ++i )
+    {
+      reader.seek(i->first,SEEK_SET);
+      while( reader.tell() <= i->second )
+	{
+	  bamrecord b = reader.next_record();
+	  samflag f(b.flag());
+	  if(!f.query_unmapped && !f.mate_unmapped)
+	    {
+	      string n = editRname(b.read_name());
+	      auto RPitr = RPlocal.find(n);
+	      if(RPitr != RPlocal.end() && 
+		 RPitr->second.first == b.refid() && 
+		 RPitr->second.second == b.pos())
+		{
+		  auto itr = lookup.find(b.refid());
+   		  int32_t start = b.pos(),stop=b.pos() + alignment_length(b) - 1;
+   		  auto CHROM = refTEs.find(itr->second);
+   		  if( CHROM != refTEs.end() )
+   		    {
+   		      bool hitsTE = find_if( CHROM->second.cbegin(),
+   					     CHROM->second.cend(),
+   					     [&](const teinfo & __t) {
+   					       bool A = (start >= __t.start() && start <= __t.stop());
+   					       bool B = (stop >= __t.start() && stop <= __t.stop());
+   					       return A||B;
+   					     }) != CHROM->second.cend();
+   		      if(!hitsTE)
+   			{
+   			  auto DCHROM = data->find(itr->second);
+  			  if(DCHROM == data->end())
+   			    {
+   			      data->insert( make_pair(itr->second,
+   						      vector<pair<int32_t,int8_t> >(1,make_pair(start,f.qstrand) ) ) );
+   			    }
+   			  else
+   			    {
+   			      DCHROM->second.emplace_back(make_pair(start,f.qstrand));
+   			    }
+   			}
+   		    }
+		}
+	    }
+	}
+    }
 }
