0421134d594a2eda75797f576c41b26639434907
krthornt <krthornt@uci.edu>
2015-02-13 07:19:05 -0800

    added functions for figuring out where to scan in a bamfile, given N total threads/chunks/whatnot

--- src/htslibUtils.cc
+++ src/htslibUtils.cc
@@ -1,6 +1,8 @@
 #include <htslibUtils.hpp>
 #include <htslib/bgzf.h>
 #include <sys/stat.h>
+#include <limits>
+#include <map>
 
 using namespace std;
 
@@ -26,6 +28,7 @@ vector<pair<string,pair<uint64_t,uint64_t> >> read_index(const std::string & bam
   BGZF * bam = bgzf_open(bamfilename.c_str(),"rb");
   bam_hdr_t * hdr = bam_hdr_read(bam);
 
+
   //read in the index
   hts_idx_t * idx = bam_index_load(indexfilename.c_str());
   if( idx == nullptr )
@@ -50,3 +53,97 @@ vector<pair<string,pair<uint64_t,uint64_t> >> read_index(const std::string & bam
   bgzf_close(bam);
   return rv;
 } 
+
+bamrange::bamrange( const std::vector<refrange> & ranges ) : beg( numeric_limits<uint64_t>::max() ),
+							     end( numeric_limits<uint64_t>::min() ),
+							     refid1(-1),
+							     refid2(-1),
+							     start(0),stop(0),
+							     refname1(std::string()),
+							     refname2(std::string())
+{
+  for( const refrange & r : ranges )
+    {
+      if( r.beg < beg ) {
+	beg = r.beg;
+	refid1 = r.refid;
+	start = r.start;
+	refname1 = r.refname;
+      }
+      if( r.end > end ) {
+	end = r.end;
+	refid2 = r.refid;
+	stop = r.stop;
+	refname2 = r.refname;
+      }
+    }
+}
+
+vector<bamrange> split_genome(const std::string & bamfilename,
+		  const unsigned & NTHREADS)
+{
+  hts_idx_t * idx = get_index(bamfilename);
+  BGZF * bam = bgzf_open(bamfilename.c_str(),"rb");
+  bam_hdr_t * hdr = bam_hdr_read(bam);
+
+  unsigned genome_len = 0;
+  vector<pair<pair<string,int32_t>,pair<unsigned,unsigned> > >lengths;
+  for( int32_t i = 0 ; i < hdr->n_targets ; ++i )
+    {    
+      hts_itr_t *iter = bam_itr_queryi(idx,i,0,hdr->target_len[i]);
+      if(iter->off != NULL )
+	{
+	  lengths.push_back(make_pair(make_pair(hdr->target_name[i],i),
+				      make_pair(1,hdr->target_len[i])));
+	  genome_len += hdr->target_len[i];
+	}      
+      hts_itr_destroy(iter);
+    }
+  unsigned chunksize = genome_len/(NTHREADS),chunkmainder = unsigned(genome_len%NTHREADS);
+  
+  unsigned CC=chunksize+chunkmainder;
+  unsigned j = 0;
+  
+  //Need to store these results in some container...
+  map<unsigned,vector< refrange > > chunks;
+  for(unsigned i = 0 ; j<lengths.size()&&i < NTHREADS;)
+    {
+      for( ; CC>0 && j < lengths.size() ;  )
+	{
+	  auto CLEN = lengths[j].second.second - lengths[j].second.first + 1;
+	  if( CLEN <= CC )
+	    {      
+	      hts_itr_t *iter = bam_itr_queryi(idx,lengths[j].first.second,lengths[j].second.first-1,lengths[j].second.second);
+	      chunks[i].emplace_back( refrange(iter->off->u, iter->off->v,
+					       lengths[j].first.second,lengths[j].second.first-1,lengths[j].second.second,lengths[j].first.first) );
+	      hts_itr_destroy(iter);
+	      CC -= CLEN;
+	      ++j;
+	    }
+	  else
+	    {
+	      hts_itr_t *iter = bam_itr_queryi(idx,lengths[j].first.second,lengths[j].second.first-1,lengths[j].second.first-1+CC);
+	      chunks[i].emplace_back( refrange(iter->off->u, iter->off->v,
+					       lengths[j].first.second,
+					       lengths[j].second.first-1,
+					       lengths[j].second.first-1+CC,
+					       lengths[j].first.first) );
+	      hts_itr_destroy(iter);
+	      
+	      lengths[j].second.first += CC;
+	      ++i;
+	      CC=chunksize;
+	    }
+	}
+    }
+  vector< bamrange > ranges(NTHREADS);
+  for ( const auto & __p : chunks )
+    {
+      ranges[__p.first]=bamrange(__p.second);
+    }
+  hts_idx_destroy(idx);
+  bam_hdr_destroy(hdr);
+  bgzf_close(bam);
+
+  return ranges;
+}
--- src/htslibUtils.hpp
+++ src/htslibUtils.hpp
@@ -21,4 +21,33 @@ hts_idx_t * get_index(const std::string & bamfilename);
  */
 std::vector<std::pair<std::string,std::pair<std::uint64_t,std::uint64_t> >> read_index(const std::string & bamfilename);
 
+struct refrange
+{
+  std::uint64_t beg,end;
+  std::int32_t refid;
+  std::uint32_t start,stop;
+  std::string refname;
+  refrange( const std::uint64_t & __beg,
+	    const std::uint64_t & __end,
+	    const std::int32_t & __refid,
+	    const std::uint32_t & __start,
+	    const std::uint32_t & __stop,
+	    const std::string  & __refname) : beg(__beg),end(__end),refid(__refid),
+					      start(__start),stop(__stop),refname(__refname)
+  {
+  }
+};
+
+struct bamrange
+{
+  std::uint64_t beg,end;
+  std::int32_t refid1,refid2;
+  std::uint32_t start,stop;
+  std::string refname1,refname2;
+  bamrange() = default;
+  bamrange( const std::vector<refrange> & ranges );
+};
+
+std::vector<bamrange> split_genome(const std::string & bamfilename,
+				   const unsigned & NTHREADS);
 #endif
